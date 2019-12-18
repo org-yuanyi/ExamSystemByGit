85c6d32c7f2bab40f5da89f43fc35f5423673327
liutao62 <627224125@qq.com>
2019-12-18 12:50:34 +0800

    新增lombok注解说明

--- src/org/aptech/pojo/QuestionInfo.java
+++ src/org/aptech/pojo/QuestionInfo.java
@@ -1,21 +1,20 @@
 package org.aptech.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-// 提供ger、set、tostring、EqualsAndHashCode
+/*
+    @Data ：getter、setter、toString、EqualsAndHashCode 的集合
+    @Setter : Target : 1.type  用于类上。为每个字段都提供 setter 方法
+                       2.field 用于字段上，为当前字段提供 setter 方法
+    @Getter ：同 Setter
+    @toString ：为当前类提供 toString 方法
+    @EqualsAndHashCode ： 提供 equals 和 hashCode 方法
+    @AllArgsConstructor : 全参构造器
+    @NoArgsConstructor : 午餐构造器
+    @RequiredArgsConstructor : 会生成一个包含常量，和标识了NotNull的变量 的构造方法。生成的构造方法是private，
+                               如何想要对外提供使用可以使用staticName选项生成一个static方法
+ */
 @Data
-@ToString
-@Getter
-@Setter
-@EqualsAndHashCode
-@AllArgsConstructor
-@RequiredArgsConstructor
 public class QuestionInfo {
     private Integer qId;                // 试题id
     private String qInfo;                // 试题名称
@@ -31,6 +30,4 @@ public class QuestionInfo {
     private String optionD;            //选项D
     private int taoTiId;                //所属套题
     private String qAnalytic;            //答案解析
-
-
 }
